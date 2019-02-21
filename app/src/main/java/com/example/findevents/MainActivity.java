6ee76440abfbd7c610bd74bfe71035a115670402
mouktar-hassan <mouktar.hassan.farah@outlook.fr>
2019-02-21 12:23:28 +0100

    Ajoute EventActivity qui est rédirigable à partir du bouton rose(+) situé en bas du l'activité Accueil!

--- app/src/main/java/com/example/findevents/MainActivity.java
+++ app/src/main/java/com/example/findevents/MainActivity.java
@@ -28,8 +28,11 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+               Intent addEvent =new Intent(MainActivity.this, EventActivity.class);
+               startActivity(addEvent);
+
             }
         });
 
