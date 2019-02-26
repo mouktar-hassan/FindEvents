6f13a2135295e7711a9f4a799fdef16c0be9a134
mouktar-hassan <mouktar.hassan.farah@outlook.fr>
2019-02-26 10:50:19 +0100

    Réctification du formulaire d'ajoute d'Évènement!

--- app/src/main/java/com/example/findevents/EventActivity.java
+++ app/src/main/java/com/example/findevents/EventActivity.java
@@ -1,60 +1,36 @@
 package com.example.findevents;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 public class EventActivity extends AppCompatActivity {
 
-    private EditText titleEvent;
-    private EditText description;
-    private EditText dateCreation;
-    private EditText dateEvent;
-    private ImageButton position;
-    private Button ajouter;
-    private Button annuler;
-    private Button events;//voir tous les evenements
+    // UI references.
+    private AutoCompleteTextView mTitle;
+    private EditText mDescription;
+    private EditText mDateCreation;
+    private EditText mDateEvent;
+    private EditText mAdresse;
+    private View mProgressView;
+    private View mLoginFormView;
+    private Button bAddEvent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        titleEvent= findViewById(R.id.editText);
-        description = findViewById(R.id.editText2);
-        dateCreation = findViewById(R.id.editText3);
-        dateEvent = findViewById(R.id.editText4);
-        position = findViewById(R.id.imageButton);
-        ajouter = findViewById(R.id.button);
-        annuler = findViewById(R.id.button2);
-        events = findViewById(R.id.button3);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        events.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
-                Intent allEvent =new Intent(EventActivity.this, MapsActivity.class);
-                startActivity(allEvent);
-
-            }
-        });
+        // Set up the AddEvent form.
+        mTitle=(AutoCompleteTextView)findViewById(R.id.eventTitle);
+        mDescription=(EditText)findViewById(R.id.eventDescription);
+        mDateCreation=(EditText)findViewById(R.id.eventDateCreation);
+        mDateEvent=(EditText)findViewById(R.id.eventDateEvent);
+        mAdresse=(EditText)findViewById(R.id.eventAdresse);
+        bAddEvent=(Button)findViewById(R.id.addEventbutton);
 
     }
-
 }
