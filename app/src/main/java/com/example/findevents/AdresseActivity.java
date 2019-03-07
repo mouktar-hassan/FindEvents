eb94ef4af7f8870c8d5a9a564edf7d3736ed645b
mouktar-hassan <mouktar.hassan.farah@outlook.fr>
2019-03-07 21:58:08 +0100

    Affichages de la liste des évènements sur ShowEventsListActivity à partir de l'URL https://api.myjson.com/bins/zng4y

--- app/src/main/java/com/example/findevents/AdresseActivity.java
+++ app/src/main/java/com/example/findevents/AdresseActivity.java
@@ -3,18 +3,33 @@ package com.example.findevents;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AdresseActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private Button click;
+    public  static TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adresse);
 
+        click = (Button) findViewById(R.id.button5);
+        data = (TextView) findViewById(R.id.textView3);
+
+
+
+    }
+
+    public void jsononClick(View view){
+        JSONParser parser =new JSONParser();
+        parser.execute();
 
     }
 }
--- app/src/main/java/com/example/findevents/ShowEventDetailActivity.java
+++ app/src/main/java/com/example/findevents/ShowEventDetailActivity.java
@@ -1,19 +1,29 @@
 package com.example.findevents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ShowEventDetailActivity extends AppCompatActivity {
 
     private ImageButton mDetailBackBtn;
+    private TextView textViewTitle;
+    private String title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_event_detail);
         mDetailBackBtn=(ImageButton)findViewById(R.id.imageBtnBackDetail);
+        textViewTitle=(TextView)findViewById(R.id.tvEventTitle);
+
+        /*Intent myintent=getIntent();
+        title=myintent.getStringExtra("ti");
+        textViewTitle.setText(title);*/
     }
 
     public void detailBackonClick(View view){
